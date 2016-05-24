165f492a150da95cd769eec9a9a7ed72114eaf2c
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2016-05-24 20:55:48 +0200

    New modes, indenting and more

--- init.el
+++ init.el
@@ -20,14 +20,14 @@
 
 ;; Write backup files to own directory
 (setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
+  `(("." . ,(expand-file-name
+    (concat user-emacs-directory "backups")))))
 
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
 
 (setq auto-save-file-name-transforms
-         `((".*" ,temporary-file-directory t)))
+  `((".*" ,temporary-file-directory t)))
 
 ;; BORING: Ensure everything is UTF-8 all the time
 (prefer-coding-system 'utf-8)
@@ -37,7 +37,7 @@
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (setq-default buffer-file-coding-system 'utf-8)
- 
+
 ;; Treat clipboard input as UTF-8 string first; compound text next, etc.
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
@@ -59,29 +59,67 @@
 
 ;; Automatically install a bunch of useful packages. You should look read up about these.
 (setq my-packages
-      '(
-	ido
+      '(ido
 	json
 	js2-mode
-        js2-refactor
+	js2-refactor
 	magit
-        emmet-mode
 	undo-tree
-        auto-complete
+	auto-complete
 	clojure-mode
 	cider
 	expand-region
 	multiple-cursors
 	markdown-mode
-        neotree
+	neotree
 	color-theme-solarized
 	paredit
 	yasnippet
+	web-mode
+	flycheck
+	json-mode
+	exec-path-from-shell
+	less-css-mode
+	find-file-in-project
+	editorconfig
 	))
 
 (dolist (package my-packages)
   (unless (package-installed-p package)
     (package-install package)))
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; Set column widths to 80, not 72
+(setq fill-column 80)
+
+;; Setup editorconfig
+(require 'editorconfig)
+(editorconfig-mode 1)
+
+;; Never insert tabs
+(set-default 'indent-tabs nil)
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq-default js2-basic-offset 2)
+
+;; Show me empty lines after buffer end
+(set-default 'indicate-empty-lines t)
+
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Display line number column
+(setq global-linum-mode t)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
@@ -89,3 +127,16 @@
 (load-theme 'solarized t)
 (global-undo-tree-mode)
 (ido-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(safe-local-variable-values (quote ((tab-width 2)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
