3b7a8e13ae6b6b603071b2d96ef4b41237447bf3
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2017-08-07 09:49:09 +0200

    Add tide setup

---
+++ settings/setup-tide.el
@@ -0,0 +1,19 @@
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+
+(setq tide-format-options '(:indentSize 2))
+
+
+	
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+(provide 'setup-tide)
