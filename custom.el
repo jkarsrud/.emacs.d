6ae512f3a913de2edbea160ef9856cb3ba33a27a
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2018-11-09 11:58:16 +0100

    Move custom-set-variables to custom.el

---
+++ custom.el
@@ -0,0 +1,55 @@
+;; CUSTOM
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(flycheck-javascript-flow-args nil)
+ '(frame-background-mode nil)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(magit-commit-arguments (quote ("--gpg-sign=328E6F61064CF7CD")))
+ '(magit-diff-use-overlays nil)
+ '(package-selected-packages
+	 (quote
+		(flycheck-color-mode-line magithub elcord rainbow-delimiters ample-theme solarized-theme lsp-javascript-flow company-flow company-lsp lsp-ui company-mode lsp-javascript-typescript lsp-mode cypher-mode add-node-modules-path company prettier-js elixir-mode flycheck-flow tide rjsx-mode coffee-mode nginx-mode nginx yaml-mode web-mode wakatime-mode undo-tree swiper paredit neotree markdown-mode magit less-css-mode json-mode js2-refactor flycheck-pos-tip find-file-in-project expand-region exec-path-from-shell emojify emmet-mode editorconfig csharp-mode color-theme-solarized cider auto-complete angular-snippets)))
+ '(safe-local-variable-values (quote ((js2-basic-offset . 2))))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#dc322f")
+		 (40 . "#c9485ddd1797")
+		 (60 . "#bf7e73b30bcb")
+		 (80 . "#b58900")
+		 (100 . "#a5a58ee30000")
+		 (120 . "#9d9d91910000")
+		 (140 . "#9595943e0000")
+		 (160 . "#8d8d96eb0000")
+		 (180 . "#859900")
+		 (200 . "#67119c4632dd")
+		 (220 . "#57d79d9d4c4c")
+		 (240 . "#489d9ef365ba")
+		 (260 . "#3963a04a7f29")
+		 (280 . "#2aa198")
+		 (300 . "#288e98cbafe2")
+		 (320 . "#27c19460bb87")
+		 (340 . "#26f38ff5c72c")
+		 (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(wakatime-python-bin nil)
+ '(weechat-color-list
+	 (quote
+		(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+	 ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+	 ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
