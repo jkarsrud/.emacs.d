c869630384a108a20eb54637fc4f839737a9f87f
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2019-01-21 13:49:34 +0100

    Company mode tweaks

--- settings/setup-company-mode.el
+++ settings/setup-company-mode.el
@@ -3,8 +3,9 @@
 (require 'company-box)
 
 (setq company-idle-delay 0.2
-			company-tooltip-limit 10
-      company-tooltip-align-annotations t)
+			company-tooltip-limit 16
+      company-tooltip-align-annotations t
+			company-tooltip-minimum-width 40)
 
 (add-hook 'after-init-hook 'global-company-mode)
 ;;(add-hook 'company-mode-hook 'company-box-mode)
