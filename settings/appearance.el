8f4f61f8e9f811dbd67f522e1c81294ca50b6592
Jesper Haug Karsrud <jkarsrud@users.noreply.github.com>
2023-02-07 19:31:49 +0100

    Reinitialize init.el from scratch (#1)
    * A lot of new stuff
    
    * Re-add theme switching in load-theme directory
    
    * Add and diminish fira-code-mode
    
    * Fix font setup and global line numbers

---
+++ settings/appearance.el
@@ -0,0 +1,35 @@
+(setq font-lock-maximum-decoration t
+      color-theme-is-global t
+      truncate-partial-width-windows nil)
+
+;; Don't beep. Don't visible-bell (fails on el capitan). Just blink the modeline on errors.
+
+(setq visible-bell nil)
+(setq ring-bell-function (lambda ()
+                           (invert-face 'mode-line)
+                           (run-with-timer 0.05 nil 'invert-face 'mode-line)))
+
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Set custom theme path
+(setq custom-theme-directory (concat user-emacs-directory "themes"))
+
+(dolist
+    (path (directory-files custom-theme-directory t "\\w+"))
+  (when (file-directory-p path)
+    (add-to-list 'custom-theme-load-path path)))
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (blink-cursor-mode -1))
+
+(setq-default line-spacing 2)
+
+(provide 'appearance)
