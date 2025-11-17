d886fc6660748c272201889f5ad2b0fb7cf2df57
Jesper Haug Karsrud <we-3PSopraJHK@autosync.no>
2025-11-17 10:15:25 +0100

    Fix calling wrong font installation function

--- settings/setup-modeline.el
+++ settings/setup-modeline.el
@@ -4,6 +4,6 @@
 (when (display-graphic-p)
   (if (find-font(font-spec :family "Symbols Nerd Font Mono"))
       (message "nerd-icons installed")
-    (all-the-icons-install-fonts t)))
+    (nerd-icons-install-fonts t)))
 
 (provide 'setup-modeline)
