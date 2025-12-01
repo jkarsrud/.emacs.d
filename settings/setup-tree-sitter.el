42af35e7b60488ce232980e6de9421316625d10b
Jesper Haug Karsrud <we-3PSopraJHK@autosync.no>
2025-12-01 22:27:40 +0100

    Lock some treesitter grammar versions
    Emacs 30.2 uses treesitter ABI v14. Will need to recompile myself if I want 15
    support, and that's more than I feel like doing right now

--- settings/setup-tree-sitter.el
+++ settings/setup-tree-sitter.el
@@ -5,11 +5,10 @@
 ;;(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 (setq treesit-language-source-alist
-      '(
-        (bash "https://github.com/tree-sitter/tree-sitter-bash")
-        (css "https://github.com/tree-sitter/tree-sitter-css")
-        (html "https://github.com/tree-sitter/tree-sitter-html")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (css "https://github.com/tree-sitter/tree-sitter-css" "v0.23.0")
+        (html "https://github.com/tree-sitter/tree-sitter-html" "v0.23.2")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.23.1" "src")
         (json "https://github.com/tree-sitter/tree-sitter-json")
         (python "https://github.com/tree-sitter/tree-sitter-python")
         (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
